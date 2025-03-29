f9f3c6f363b9aa4d490588490c5df21b5d15baf3
Zaur Gokoti <z.kokoev@gmail.com>
2025-03-29 13:16:23 +0000

    app_settings_added

---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from .settings import get_settings, settings_map
---
+++ app/settings.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+
+# All modes
+@dataclass()
+class BaseSettings:
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = int(os.getenv("POSTGRES_PORT"))
+
+
+class DevSettings(BaseSettings):
+    pass
+
+
+class TestSettings(BaseSettings):
+    pass
+
+
+class ProdSettings(BaseSettings):
+    pass
+
+
+settings_map = {
+    "DEV": DevSettings,
+    "TEST": TestSettings,
+    "PROD": ProdSettings
+}
+
+
+def get_settings(env="DEV"):
+    return settings_map[env]()
